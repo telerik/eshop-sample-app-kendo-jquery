738d2c1ea92f98328a4b805900fbb75ddc661fd4
Neli Kondova <kondova@progress.com>
2023-01-17 19:54:13 +0200

    remove redundant code and add some styles

--- EShop/Models/ViewModels/FavoriteReportUserViewModel.cs
+++ EShop/Models/ViewModels/FavoriteReportUserViewModel.cs
@@ -1,9 +1,7 @@
 ﻿namespace Models.ViewModels
 {
     public class FavoriteUserProductViewModel
-    {
-        //public int Id { get; set; }      
-
+    {    
         public string UserName { get; set; }
       
         public List<FavoriteReportProductViewModel> FavoriteReportProducts { get; set; }
