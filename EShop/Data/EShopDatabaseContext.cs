3f39cd05c51f9c196b681076e5fdf50368b86d83
ag-petrov <angel.petrov@progress.com>
2024-06-05 11:00:27 +0300

    chore: modify DB Context to use SQLlite

--- EShop/Data/EShopDatabaseContext.cs
+++ EShop/Data/EShopDatabaseContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Options;
 
 namespace Data
 {
@@ -27,7 +28,9 @@ namespace Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=KendoEShop;Trusted_Connection=True; MultipleActiveResultSets=true");
+                var dataDirectory = Path.GetFullPath("wwwroot");
+
+                optionsBuilder.UseSqlite(@"Data Source=" + dataDirectory + System.IO.Path.DirectorySeparatorChar + @"demos.db;");
             }
         }
 
